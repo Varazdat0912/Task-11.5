a36f7851bf2c9b11d55165bf1622517c80cea0c2
Varazdat0912 <122081143+Varazdat0912@users.noreply.github.com>
2023-01-23 18:53:00 +0300

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const { sequelize } = require("../DB");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define(
+  "User",
+  {
+    // Модели атрибутов
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    photo_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    birthday: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    gender: {
+      type: DataTypes.ENUM(["male", "female"]),
+      allowNull: true,
+    },
+    balance: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      defaultValue: 0,
+    },
+  },
+  {
+    // Другие модели
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+module.exports = { User };
